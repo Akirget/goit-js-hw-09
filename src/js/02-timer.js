f9be69e034b6db538a6065a98efff4f8601c5c8d
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-09-27 16:10:18 +0200

    Домашнє завдання JS №9

---
+++ src/js/02-timer.js
@@ -0,0 +1,29 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const buttonStart = document.querySelector('[data-start]');
+const day = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+let timerId = null;
+
+buttonStart.setAttribute('disabled', true);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+
+  const hours = Math.floor((ms % day) / hour);
+
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
